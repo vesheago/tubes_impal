e14c9148356879f52404fafc8e59d94b310af5b8
farissh <49398418+farissh@users.noreply.github.com>
2019-09-03 01:26:37 +0700

    update tabel tagihan

--- db_impal.sql
+++ db_impal.sql
@@ -182,6 +182,40 @@ ALTER TABLE `laporan`
   ADD CONSTRAINT `laporan_ibfk_1` FOREIGN KEY (`id_manajer_FK`) REFERENCES `manajer` (`id_manajer`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
+--
+-- Table structure for table `tagihan`
+--
+
+CREATE TABLE `tagihan` (
+  `id_tagihan` varchar(4) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL,
+  `no_antrean` varchar(3) NOT NULL,
+  `nopol` varchar(8) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `total` int(10) NOT NULL,
+  `id_admin` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tagihan`
+--
+
+INSERT INTO `tagihan` (`id_tagihan`, `tanggal`, `jam`, `no_antrean`, `nopol`, `status`, `total`, `id_admin`) VALUES
+('B010', '2019-07-19', '13:10:00', 'X01', 'D137AA', 'Belum Lunas', 300000, 'A002'),
+('B011', '2019-08-24', '08:05:00', 'X02', 'AD011D', 'Belum Lunas', 110000, 'A001'),
+('B012', '2019-10-10', '10:14:00', 'X03', 'E111A', 'Lunas', 90000, 'A003');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  ADD PRIMARY KEY (`id_tagihan`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
