42e782ce72772f39daaa7681681a0f3e871ae91e
vesheago <vesheafg@gmail.com>
2019-09-15 14:06:55 +0700

    buat session id dan nama untuk muncul di page index dan header

--- application/controllers/C_login.php
+++ application/controllers/C_login.php
@@ -19,27 +19,54 @@ class C_login extends CI_Controller{
 				if($cek_admin->num_rows() > 0){
 					$data=$cek_admin->row_array();
         			$this->session->set_userdata('masuk',TRUE);
-					$this->session->set_userdata('ses_id',$data['id']);
-					$this->session->set_userdata('ses_nama',$data['nama']);
+					$this->session->set_userdata('ses_id',$data['id_admin']);
+					$this->session->set_userdata('ses_nama',$data['nama_admin']);
 					redirect('index.php/C_admin');
 				}else if($cek_customer->num_rows() > 0){
-					$data=$cek_admin->row_array();
+					$data=$cek_customer->row_array();
         			$this->session->set_userdata('masuk',TRUE);
-					$this->session->set_userdata('ses_id',$data['id']);
-					$this->session->set_userdata('ses_nama',$data['nama']);
+					$this->session->set_userdata('ses_id',$data['id_customer']);
+					$this->session->set_userdata('ses_nama',$data['nama_customer']);
 					redirect('index.php/C_customer');
 				}else if($cek_manajer->num_rows() > 0){
-					$data=$cek_admin->row_array();
+					$data=$cek_manajer->row_array();
         			$this->session->set_userdata('masuk',TRUE);
-					$this->session->set_userdata('ses_id',$data['id']);
-					$this->session->set_userdata('ses_nama',$data['nama']);
+					$this->session->set_userdata('ses_id',$data['id_manajer']);
+					$this->session->set_userdata('ses_nama',$data['nama_manajer']);
 					redirect('index.php/C_manajer');
 				}else{  // jika username dan password tidak ditemukan atau salah
 					$url=base_url();
 					echo $this->session->set_flashdata('msg','Username Atau Password Salah');
 					redirect($url);
 				}
-			}	
+	}
+	
+	public function login(){
+		$username = $this->input->post('username'); // Ambil isi dari inputan username pada form login
+		$password = md5($this->input->post('password')); // Ambil isi dari inputan password pada form login dan encrypt dengan md5
+	
+		$user = $this->UserModel->get($username); // Panggil fungsi get yang ada di UserModel.php
+	
+		if(empty($user)){ // Jika hasilnya kosong / user tidak ditemukan
+		  $this->session->set_flashdata('message', 'Username tidak ditemukan'); // Buat session flashdata
+		  redirect('auth'); // Redirect ke halaman login
+		}else{
+		  if($password == $user->password){ // Jika password yang diinput sama dengan password yang didatabase
+			$session = array(
+			  'authenticated'=>true, // Buat session authenticated dengan value true
+			  'username'=>$user->username,  // Buat session username
+			  'nama'=>$user->nama // Buat session authenticated
+			);
+	
+			$this->session->set_userdata($session); // Buat session sesuai $session
+			redirect('page/welcome'); // Redirect ke halaman welcome
+		  }else{
+			$this->session->set_flashdata('message', 'Password salah'); // Buat session flashdata
+			redirect('auth'); // Redirect ke halaman login
+		  }
+		}
+	  }
+
     function logout(){
         $this->session->sess_destroy();
         $url = base_url('');
--- application/views/page_login.php
+++ application/views/page_login.php
@@ -20,12 +20,13 @@
             <?php echo $this->session->flashdata('msg');?>
             <label for="username" class="sr-only">Username</label>
             <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
+            <br>
             <label for="password" class="sr-only">Password</label>
             <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
             <div class="checkbox">
-              <label>
+              <!-- <label>
                 <input type="checkbox" value="remember-me"> Remember me
-              </label>
+              </label> -->
             </div>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
           </form>
