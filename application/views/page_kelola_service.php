2329d6ef3e65f91da20446437b6c649986d8b605
milhamsetiawan <muhammadilhamsetiawan8@gmail.com>
2019-09-19 13:36:58 +0700

    halaman kelola service

--- application/views/page_kelola_service.php
+++ application/views/page_kelola_service.php
@@ -32,7 +32,7 @@
     <td colspan="6" align="center"><strong>Service Umum</strong></td>
     </tr>
   <tr>
-    <td width="156">Ganti Aki</td>
+    <td width="300">Ganti Aki</td>
     <td width="29">Rp. </td>
     <td width="99" align="right">720.000</td>
     <td width="33" align="center"><input type=checkbox name=nasi value="720000" onClick="this.form.total.value=checkChoice(this);"></td>
@@ -42,6 +42,60 @@
     <td>Rp.</td>
     <td align="right">860.000</td>
     <td align="center"><input type=checkbox name=ikan value="860000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Tune Up Mesin</td>
+    <td>Rp.</td>
+    <td align="right">245.000</td>
+    <td align="center"><input type=checkbox name=ikan value="245000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Ganti Oli Transmisi</td>
+    <td>Rp.</td>
+    <td align="right">150.000</td>
+    <td align="center"><input type=checkbox name=ikan value="150000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Tune Up Mesin + Servis Rem</td>
+    <td>Rp.</td>
+    <td align="right">345.000</td>
+    <td align="center"><input type=checkbox name=ikan value="345000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Pengecekan Masalah</td>
+    <td>Rp.</td>
+    <td align="right">100.000</td>
+    <td align="center"><input type=checkbox name=ikan value="100000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Inspeksi 150 Poin</td>
+    <td>Rp.</td>
+    <td align="right">345.000</td>
+    <td align="center"><input type=checkbox name=ikan value="345000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Kuras Air Radiator / Coolant</td>
+    <td>Rp.</td>
+    <td align="right">69.000</td>
+    <td align="center"><input type=checkbox name=ikan value="69000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Kuras Minyak Rem</td>
+    <td>Rp.</td>
+    <td align="right">105.000</td>
+    <td align="center"><input type=checkbox name=ikan value="105000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Pengecekan Masalah AC Mobil</td>
+    <td>Rp.</td>
+    <td align="right">100.000</td>
+    <td align="center"><input type=checkbox name=ikan value="100000" onClick="this.form.total.value=checkChoice(this);"></td>
+  </tr>
+  <tr>
+    <td>Servis Rem</td>
+    <td>Rp.</td>
+    <td align="right">100.000</td>
+    <td align="center"><input type=checkbox name=ikan value="100000" onClick="this.form.total.value=checkChoice(this);"></td>
   </tr>
   <tr>
     <td colspan="4" align="right">Total :
