adcf52dc5913f0bf02091c552f6a5b5ed0a93e49
milhamsetiawan <muhammadilhamsetiawan8@gmail.com>
2019-09-19 15:11:54 +0700

    page total tagihan (blm beres)

---
+++ application/views/page_total_tagihan.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<title>Tagihan</title>
+  <br>
+  <br>
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+          <h1 class="text-center">Tagihan</h1>
+          
+        </div>
+    </div>
+</body>
+</html>
